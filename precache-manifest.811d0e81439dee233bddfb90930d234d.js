c180e72a2bd57322fbf30cf01c87bc2e2bbfb868
Deep Gamit <gamit.d@husky.neu.edu>
2020-08-26 19:51:35 -0400

    Updates

---
+++ precache-manifest.811d0e81439dee233bddfb90930d234d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b13c0651244b6f1d3438ff1bb485da",
+    "url": "/Movie-Nomination/index.html"
+  },
+  {
+    "revision": "8fbce3a3b41afa56c9b9",
+    "url": "/Movie-Nomination/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5b767c15018f89fa60a4",
+    "url": "/Movie-Nomination/static/css/main.a81ef400.chunk.css"
+  },
+  {
+    "revision": "8fbce3a3b41afa56c9b9",
+    "url": "/Movie-Nomination/static/js/2.e187cd87.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Movie-Nomination/static/js/2.e187cd87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b767c15018f89fa60a4",
+    "url": "/Movie-Nomination/static/js/main.8d68b080.chunk.js"
+  },
+  {
+    "revision": "aa7dbd1f2be36fbdc9a9",
+    "url": "/Movie-Nomination/static/js/runtime-main.ed2dcf00.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/Movie-Nomination/static/media/loading.26ef8bb4.gif"
+  }
+]);
